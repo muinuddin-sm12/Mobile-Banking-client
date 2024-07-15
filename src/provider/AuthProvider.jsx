a9630562148646f13ffec3ca28dfd4f8e82d77be
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-07-15 23:00:53 +0400

    add: firebase init & environment variable

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,59 +1,55 @@
 /* eslint-disable react/prop-types */
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from "react";
 import {
-  GoogleAuthProvider,
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
-  signInWithPopup,
   signOut,
   updateProfile,
-} from 'firebase/auth'
-import { auth } from '../firebase/firebase.init'
-import axios from 'axios'
-// import { app } from '../firebase/firebase.init'
-
-export const AuthContext = createContext(null)
-// const auth = getAuth(app)
-const googleProvider = new GoogleAuthProvider()
+} from "firebase/auth";
+import { auth } from "../firebase/firebase.init";
+import axios from "axios";
+export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
-    setLoading(true)
-    return createUserWithEmailAndPassword(auth, email, password)
-  }
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
   const signIn = (email, password) => {
-    setLoading(true)
-    return signInWithEmailAndPassword(auth, email, password)
-  }
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const logOut = async () => {
-    setLoading(true)
-    await axios('https://edu-circle-server.vercel.app/logout', {withCredentials:true})
-    return signOut(auth)
-  }
+    setLoading(true);
+    await axios("https://edu-circle-server.vercel.app/logout", {
+      withCredentials: true,
+    });
+    return signOut(auth);
+  };
 
   const updateUserProfile = (name, photo) => {
     return updateProfile(auth.currentUser, {
       displayName: name,
       photoURL: photo,
-    })
-  }
+    });
+  };
 
   // onAuthStateChange
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, currentUser => {
-      setUser(currentUser)
-      setLoading(false)
-    })
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
     return () => {
-      return unsubscribe()
-    }
-  }, [])
+      return unsubscribe();
+    };
+  }, []);
 
   const authInfo = {
     user,
@@ -64,11 +60,11 @@ const AuthProvider = ({ children }) => {
     signIn,
     logOut,
     updateUserProfile,
-  }
+  };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,3 +1,7 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+
 const PrivateRoutes = ({children}) => {
     const { user, loading } = useContext(AuthContext);
   const location = useLocation();
